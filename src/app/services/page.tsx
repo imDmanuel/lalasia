2caab6876c9ba3a2f5739c0cdda65ab8ae4d3bd6
Albinson Normanson <chiwuzoemmanuel@gmail.com>
2023-07-26 00:12:28 -0700

    feat:worked on services page, completed responsiveness also

---
+++ src/app/services/page.tsx
@@ -0,0 +1,176 @@
+import Image from "next/image";
+import heroImage from "@/assets/images/services-hero-image.png";
+import featuredImage1 from "@/assets/images/service-featured-1.png";
+import featuredImage2 from "@/assets/images/service-featured-2.png";
+import featuredImage3 from "@/assets/images/service-featured-3.png";
+import arrowRightIcon from "@/assets/images/arrow-right-icon.svg";
+import Link from "next/link";
+
+const ServicesPage: React.FC = function () {
+  return (
+    <>
+      <header className="lg:mt-24 mt-12">
+        <div className="container">
+          <h1 className="text-center max-lg:text-2xl">Services</h1>
+
+          <p className="mt-5 mb-12 max-w-lg mx-auto text-center">
+            The product crafted by talented crafter and using high quality
+            material with love inside
+          </p>
+
+          <Image className="w-full" src={heroImage} alt="" />
+        </div>
+      </header>
+      <main>
+        <section className="mt-24">
+          <div className="container">
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-7">
+              <ServiceItem
+                index="01"
+                name="Furniture"
+                description="At the ultimate smart home, you're met with technology before you even step through the front door. "
+              />
+              <ServiceItem
+                index="02"
+                name="Home Decoration"
+                description="Create A Calming Summer Escape With Our Luxurious Home Accessories For The Balmy Evenings."
+              />
+              <ServiceItem
+                index="03"
+                name="Kitchen Cabinet"
+                description="Introducing the modular kitchen cabinet system. Start with our huge selection of base and wall cabinets. "
+              />
+              <ServiceItem
+                index="04"
+                name="Interior Design"
+                description="Innovative products often feature innovative designs that play with the patterns we are familiar."
+              />
+              <ServiceItem
+                index="05"
+                name="Exterior Design"
+                description="These stylish and resilient products provide up-to-date options for roofing, siding, decking, and outdoor spaces."
+              />
+              <ServiceItem
+                index="06"
+                name="Custom Furniture"
+                description="With Quality Materials and Modern Designs, we have Designs for all Tastes. we bring you World Class Designs."
+              />
+            </div>
+          </div>
+        </section>
+
+        <section className="mt-12 xl:mt-44">
+          <div className="container">
+            <div className="text-lg font-bold text-secondary-600">
+              Portofolio
+            </div>
+            <div className="flex flex-col xl:flex-row justify-between xl:gap-10 mt-2 mb-4 xl:mb-12">
+              <h2 className="max-w-lg max-xl:text-2xl">
+                Amazing project we’ve done before
+              </h2>
+
+              <div className="max-w-lg">
+                <p className="mb-5 max-xl:text-sm">
+                  Pellentesque etiam blandit in tincidunt at donec. Eget ipsum
+                  dignissim placerat nisi, adipiscing mauris non.
+                </p>
+                <Link
+                  className="text-primary-600 font-bold text-sm xl:text-lg"
+                  href={"#"}
+                >
+                  View Portfolio
+                </Link>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-1 xl:grid-cols-[4.174fr,5.826fr] grid-rows-2 gap-7">
+              <div className="row-span-2 relative">
+                {/* TODO: CHANGE IMAGE IN SMALLER SCREEN SIZES, THAT BRINGS OUT MORE DETAILS */}
+                <Image
+                  className="w-full h-full object-cover"
+                  src={featuredImage1}
+                  alt=""
+                />
+                <div className="absolute bottom-0 right-0 left-0 px-5 xl:px-7 pb-5 xl:pb-7 max-w-md">
+                  <h3 className="text-white max-xl:text-sm">
+                    Siska Kohl’s Bedroom
+                  </h3>
+                  <p className="text-white truncate mt-1 mb-3">
+                    We start renovating her bedroom with minimalist concept and
+                    using combination white and wooden material
+                  </p>
+                  <Link className="text-white" href={"#"}>
+                    See Detail
+                  </Link>
+                </div>
+              </div>
+              <div className="relative">
+                {/* TODO: ADD IMAGE GRADIENT OVERLAY */}
+                <Image className="w-full h-full" src={featuredImage2} alt="" />
+                <div className="absolute bottom-0 right-0 left-0 px-5 xl:px-7 pb-5 xl:pb-7 pt-40 max-w-md">
+                  <h3 className="text-white max-xl:text-sm">
+                    Jeruk Veldevana Living Room Design
+                  </h3>
+                  <p className="text-white truncate mt-1 mb-3">
+                    We start renovating her bedroom with minimalist concept and
+                    using combination white and wooden material
+                  </p>
+                  <Link className="text-white" href={"#"}>
+                    See Detail
+                  </Link>
+                </div>
+              </div>
+              <div className="relative">
+                {/* TODO: ADD IMAGE GRADIENT OVERLAY */}
+                <Image className="w-full h-full" src={featuredImage3} alt="" />
+                <div className="absolute bottom-0 right-0 left-0 px-5 xl:px-7 pb-5 xl:pb-7 pt-40 max-w-md">
+                  <h3 className="text-white max-xl:text-sm">
+                    Cozy Co-working space
+                  </h3>
+                  <p className="text-white truncate mt-1 mb-3">
+                    We start renovating her bedroom with minimalist concept and
+                    using combination white and wooden material
+                  </p>
+                  <Link className="text-white" href={"#"}>
+                    See Detail
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="mt-12 xl:mt-44 mb-12 xl:mb-20">
+          <div className="container flex md:flex-row flex-col space-y-4 md:space-y-0 justify-between">
+            <h2 className="max-w-[30rem] max-xl:text-2xl">
+              Are you interested in working with us?
+            </h2>
+
+            <div>
+              <button className="btn btn-primary flex items-center gap-3 min-w-fit">
+                <div>Let&apos;s Talk</div>
+                <Image src={arrowRightIcon} alt="" />
+              </button>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+const ServiceItem: React.FC<{
+  index: string;
+  name: string;
+  description: string;
+}> = function ({ index, name, description }) {
+  return (
+    <div>
+      <div className="text-3xl xl:text-[4rem] text-primary-600">{index}</div>
+      <div className="header-3 max-xl:text-sm">{name}</div>
+      <p className="max-xl:text-xs">{description}</p>
+    </div>
+  );
+};
+
+export default ServicesPage;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,75 +5,77 @@ import Link from "next/link";
 export const Footer: React.FC = function () {
   return (
     <footer className="xl:mt-20 mt-12">
-      <div className="container flex xl:flex-row flex-col border-t border-screen-line xl:pt-20 xl:pb-20 pt-5 pb-6">
-        {/* Left section */}
-        <div>
-          {/* Logo */}
-          <div className="w-24 h-8 xl:w-32 xl:h-10">
-            <Image className="object-cover" src={logoImage} alt="Logo" />
-          </div>
-          {/* Branding Summary */}
-          <p className="text-sm xl:paragraph-3 max-w-lg mt-9">
-            Lalasia is digital agency that help you make better experience
-            iaculis cras in.
-          </p>
-        </div>
-
-        {/* Right Section */}
-        <div className="flex gap-7 xl:gap-24  xl:ml-auto ml-0 xl:mt-0 mt-7">
-          {/* Product column */}
+      <div className="container">
+        <div className="flex xl:flex-row flex-col border-t border-screen-line xl:pt-20 xl:pb-20 pt-5 pb-6">
+          {/* Left section */}
           <div>
-            <h4 className="max-xl:text-base">Product</h4>
-
-            <ul className="mt-5 space-y-3 text-sm xl:text-lg text-paragraph-600">
-              <li>
-                <Link href="#">New Arrivals</Link>
-              </li>
-              <li>
-                <Link href="#">Best Selling</Link>
-              </li>
-              <li>
-                <Link href="#">Home Decor</Link>
-              </li>
-              <li>
-                <Link href="#">Kitchen Set</Link>
-              </li>
-            </ul>
+            {/* Logo */}
+            <div className="w-24 h-8 xl:w-32 xl:h-10">
+              <Image className="object-cover" src={logoImage} alt="Logo" />
+            </div>
+            {/* Branding Summary */}
+            <p className="text-sm xl:paragraph-3 max-w-lg mt-9">
+              Lalasia is digital agency that help you make better experience
+              iaculis cras in.
+            </p>
           </div>
 
-          {/* Services column */}
-          <div>
-            <h4 className="max-xl:text-base">Services</h4>
-            <ul className="mt-5 space-y-3 text-sm xl:text-lg text-paragraph-600">
-              <li>
-                <Link href="#">Catalog</Link>
-              </li>
-              <li>
-                <Link href="#">Blog</Link>
-              </li>
-              <li>
-                <Link href="#">FaQ</Link>
-              </li>
-              <li>
-                <Link href="#">Pricing</Link>
-              </li>
-            </ul>
-          </div>
+          {/* Right Section */}
+          <div className="flex gap-7 xl:gap-24  xl:ml-auto ml-0 xl:mt-0 mt-7">
+            {/* Product column */}
+            <div>
+              <h4 className="max-xl:text-base">Product</h4>
 
-          {/* Follow Us column */}
-          <div>
-            <h4 className="max-xl:text-base">Follow Us</h4>
-            <ul className="mt-5 space-y-3 text-sm xl:text-lg text-paragraph-600">
-              <li>
-                <Link href="#">Facebook</Link>
-              </li>
-              <li>
-                <Link href="#">Instagram</Link>
-              </li>
-              <li>
-                <Link href="#">Twitter</Link>
-              </li>
-            </ul>
+              <ul className="mt-5 space-y-3 text-sm xl:text-lg text-paragraph-600">
+                <li>
+                  <Link href="#">New Arrivals</Link>
+                </li>
+                <li>
+                  <Link href="#">Best Selling</Link>
+                </li>
+                <li>
+                  <Link href="#">Home Decor</Link>
+                </li>
+                <li>
+                  <Link href="#">Kitchen Set</Link>
+                </li>
+              </ul>
+            </div>
+
+            {/* Services column */}
+            <div>
+              <h4 className="max-xl:text-base">Services</h4>
+              <ul className="mt-5 space-y-3 text-sm xl:text-lg text-paragraph-600">
+                <li>
+                  <Link href="#">Catalog</Link>
+                </li>
+                <li>
+                  <Link href="#">Blog</Link>
+                </li>
+                <li>
+                  <Link href="#">FaQ</Link>
+                </li>
+                <li>
+                  <Link href="#">Pricing</Link>
+                </li>
+              </ul>
+            </div>
+
+            {/* Follow Us column */}
+            <div>
+              <h4 className="max-xl:text-base">Follow Us</h4>
+              <ul className="mt-5 space-y-3 text-sm xl:text-lg text-paragraph-600">
+                <li>
+                  <Link href="#">Facebook</Link>
+                </li>
+                <li>
+                  <Link href="#">Instagram</Link>
+                </li>
+                <li>
+                  <Link href="#">Twitter</Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
